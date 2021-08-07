a80ce4d098aa94c0e02396f4480b0a955e6ba093
pk-x509 <panupong.x509@gmail.com>
2021-08-07 10:59:10 +0700

    -

---
+++ GFCA.APT.Domain/DTO/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace GFCA.APT.Domain.DTO
+{
+    public class UserProfile
+    {
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
---
+++ GFCA.APT.WEB/Areas/Masters/Data/ProductViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GFCA.APT.WEB.Areas.Masters.Data
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public int ProductId { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int BrandId { get; set; }
+        public string BrandCode { get; set; }
+        public string ProductCategoryName { get; set; }
+        public string ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public string SKU { get; set; }
+        public string Color { get; set; }
+        public Nullable<decimal> Width { get; set; }
+        public Nullable<decimal> Weight { get; set; }
+        public Nullable<decimal> Height { get; set; }
+        public string Shape { get; set; }
+        public string Size { get; set; }
+        public Nullable<int> QTY { get; set; }
+        public string UOM { get; set; }
+        public Nullable<decimal> LTP { get; set; }
+        public string CreatedBy { get; set; }
+        public System.DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public Nullable<System.DateTime> UpdatedDate { get; set; }
+
+    }
+}
