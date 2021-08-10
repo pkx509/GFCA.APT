5bf14baf2424846bf007023ed617fc169a3324a5
Kaiser <JirasakMooksri@hotmail.com>
2021-08-10 13:48:20 +0700

    Update

---
+++ GFCA.APT.Domain/Dto/ProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GFCA.APT.Domain.Dto
+{
+    public class BrandDto
+    {
+        [Required]
+        public int? BRAND_ID { get; set; }
+        [Required]
+        public string BRAND_CODE { get; set; }
+        public string BRAND_NAME { get; set; }
+        public string FLAG_ROW { get; set; }
+        public string CREATED_BY { get; set; }
+        public Nullable<DateTime> CREATED_DATE { get; set; }
+        public string UPDATED_BY { get; set; }
+        public Nullable<DateTime> UPDATED_DATE { get; set; }
+    }
+}
--- GFCA.APT.WEB/Areas/Masters/Controllers/ProductController.cs
+++ GFCA.APT.WEB/Areas/Masters/Controllers/ProductController.cs
@@ -1,10 +1,22 @@
-﻿using System.Web.Mvc;
+﻿using GFCA.APT.BAL.Log;
+using GFCA.APT.BAL.Parties;
+using GFCA.APT.DAL;
+using System.Web.Mvc;
+using GFCA.APT.DAL.Implements;
 
 namespace GFCA.APT.WEB.Areas.Masters.Controllers
 {
-    public class ProductController : Controller
+    public class ProductController : ControllerWebBase
     {
-        // GET: Masters/Product
+        private readonly IUnitOfWork _uow;
+        private readonly IBrandService _brandSvc;
+        public ProductController(ILogService log) : base(log)
+        {
+            _uow = new UnitOfWork();
+            _brandSvc = new BrandService(_uow, log);
+        }
+        
+      
         public ActionResult Index()
         {
             return View();
