9267a95b2f13902cba8d2572827384b9f036d889
pk-x509 <panupong.x509@gmail.com>
2021-08-22 18:12:40 +0700

    add missing CostCenterDto, UserInfoDto

---
+++ GFCA.APT.Domain/Dto/UserInfoDto.cs
@@ -0,0 +1,27 @@
+﻿
+namespace GFCA.APT.Domain.Dto
+{
+    public class UserInfoDto
+    {
+        public int? UserId { get; set; }
+        public string UserName { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public string Country { get; set; }
+        public string Region { get; set; }
+        public string Title { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Section { get; set; }
+        public string Location { get; set; }
+        public string EmployeeNo { get; set; }
+        public string Email { get; set; }
+        public string Division { get; set; }
+        public string Department { get; set; }
+        public string CompanyType { get; set; }
+        public string Company { get; set; }
+        public string Language { get; set; }
+        public string UserAlias { get; set; }
+
+    }
+}
