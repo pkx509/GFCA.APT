6795fdecf044faf5fe73d97ecf0de3ec6b511ca2
pk-x509 <panupong.x509@gmail.com>
2021-08-28 00:04:10 +0700

    add notify project

---
+++ GFCA.APT.NOTI/Interfaces/INotifies.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace GFCA.APT.NOTI.Interfaces
+{
+    public interface INotifies<T>
+    {
+        void SetContext();
+        Task<string> Send(T context);
+    }
+}
