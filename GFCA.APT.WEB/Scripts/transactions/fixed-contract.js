49cf45b9d57ab741d4d3c4980d8f399714630448
pk-x509 <panupong.x509@gmail.com>
2021-09-12 20:39:46 +0700

    example combobox changing

--- GFCA.APT.WEB/Scripts/transactions/fixed-contract.js
+++ GFCA.APT.WEB/Scripts/transactions/fixed-contract.js
@@ -1,41 +1,9 @@
-﻿;(function ($, window, document, undefined) {
-    "use strict";
+﻿let page = new (function () {
 
-    //region declare class fixed contract //
-    let fixedContract = function (element, options) {
-        let self = this;
-
-        self.element = element;
-        self.options = $.extend({}, $.fn.fixedContract.defaults, options);
-
-        $(document).on("commandSend", function () {
-            let data = {};
-            self.commandSend(data);
-        });
-
-
-        self.commandSend = function (data) {
-
-        }
-
-        //private function
-        function pageLoad() {
-
-        }
+    this.cmbPositionChange = function (e) {
+        let t = e.itemData.Text;
+        let v = e.value;
+        console.log(`value = (${v}) and text = (${t})`);
     }
-    //endregion declare class fixed contract //
 
-    $.fn.fixedContract.defaults = {
-
-    };
-
-    $.fn.fixedContract.Constructor = fixedContract;
-
-    let old = $.fn.fixedContract;
-    $.fn.fixedContract.noConflict = function () {
-        $.fn.fixedContract = old;
-        return this;
-    };
-
-
-}(jQuery, window, document));
+})();
