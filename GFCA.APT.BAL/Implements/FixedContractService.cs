772eb8e08aaff7306801ecd0051754a2a5ef590b
pk-x509 <panupong.x509@gmail.com>
2021-09-19 20:46:49 +0700

    --

---
+++ GFCA.APT.BAL/Implements/FixedContractService.cs
@@ -0,0 +1,98 @@
+﻿using GFCA.APT.BAL.Interfaces;
+using GFCA.APT.DAL.Implements;
+using GFCA.APT.DAL.Interfaces;
+using GFCA.APT.Domain.Dto;
+using GFCA.APT.Domain.Models;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFCA.APT.BAL.Implements
+{
+    public class FixedContractService: ServiceBase, IFixedContractService
+    {
+        private readonly ILog _logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        internal static FixedContractService CreateInstant()
+        {
+            var uow = UnitOfWork.CreateInstant();
+            var svc = new FixedContractService(uow);
+
+            return svc;
+        }
+
+        public FixedContractService(IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+
+        }
+
+        public EmployeeDto GetHeaderById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeDto> GetDeatilByHeaderId(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public BusinessResponse Create(EmployeeDto model)
+        {
+            try
+            {
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public BusinessResponse Edit(EmployeeDto model)
+        {
+            try
+            {
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public IEnumerable<EmployeeDto> GetHeaderAll()
+        {
+            try
+            {
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public BusinessResponse Remove(EmployeeDto model)
+        {
+            try
+            {
+
+                throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+    }
+}
---
+++ GFCA.APT.BAL/Interfaces/IFixedContractService.cs
@@ -0,0 +1,20 @@
+﻿using GFCA.APT.Domain.Dto;
+using GFCA.APT.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFCA.APT.BAL.Interfaces
+{
+    public interface IFixedContractService
+    {
+        IEnumerable<EmployeeDto> GetHeaderAll();
+        EmployeeDto GetHeaderById(int Id);
+        IEnumerable<EmployeeDto> GetDeatilByHeaderId(int Id);
+        BusinessResponse Create(EmployeeDto model);
+        BusinessResponse Edit(EmployeeDto model);
+        BusinessResponse Remove(EmployeeDto model);
+    }
+}
