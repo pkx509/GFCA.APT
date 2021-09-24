ac405a2c93e72b13af4aed56351ce223100ba69e
pk-x509 <panupong.x509@gmail.com>
2021-09-24 11:07:42 +0700

    Fixing SizeRepository after merge

--- GFCA.APT.DAL/Implements/SizeRepository.cs
+++ GFCA.APT.DAL/Implements/SizeRepository.cs
@@ -9,113 +9,42 @@ namespace GFCA.APT.DAL.Implements
 {
     public class SizeRepository : RepositoryBase, ISizeRepository
     {
-
+        
         public SizeRepository(IDbTransaction transaction): base(transaction) { }
 
-        public SizeDto GetByCode(string code)
-        {
-            string sqlQuery = @"SELECT * FROM TB_M_SIZE WHERE SIZE_CODE = @SIZE_CODE;";
-            var query = Connection.Query<SizeDto>(
-                sql: sqlQuery,
-                param: new { SIZE_CODE = code }
-                ,transaction: Transaction
-                ).FirstOrDefault();
        
-            return query;
-        }
+
         public IEnumerable<SizeDto> All()
         {
-            string sqlQuery = @"SELECT * FROM TB_M_SIZE";
+            string sqlQuery = @"SELECT * FROM TB_M_SIZE;";
             var query = Connection.Query<SizeDto>(
                 sql: sqlQuery
-                ,transaction: Transaction
+                , transaction: Transaction
                 ).ToList();
 
             return query;
         }
 
+
+        public void Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public SizeDto GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public void Insert(SizeDto entity)
         {
-            string sqlExecute = @"INSERT INTO TB_M_SIZE
-                                (
-                                  SIZE_CODE
-                                , SIZE_NAME
-                                , SIZE_DESC
-                                , FLAG_ROW
-                                , CREATED_BY
-                                , CREATED_DATE
-                                ) VALUES (
-                                  @SIZE_CODE
-                                , @SIZE_NAME
-                                , @SIZE_DESC
-                                , @FLAG_ROW
-                                , @CREATED_BY
-                                , @CREATED_DATE
-                                ); SELECT SCOPE_IDENTITY()
-                                ";
-
-            var parms = new
-            {
-                SIZE_CODE = entity.SIZE_CODE,
-                SIZE_NAME = entity.SIZE_NAME,
-                SIZE_DESC = entity.SIZE_DESC,
-                FLAG_ROW = entity.FLAG_ROW,
-                CREATED_BY = entity.CREATED_BY,
-                CREATED_DATE = entity.CREATED_DATE?.ToDateTime2(),
-            };
-
-            Connection.ExecuteScalar<int>(
-                sql: sqlExecute,
-                param: parms,
-                transaction: Transaction
-            );
-
+            throw new System.NotImplementedException();
         }
+
         public void Update(SizeDto entity)
         {
-            string sqlExecute = @"UPDATE TB_M_SIZE
-                                SET
-                                  SIZE_CODE   = @SIZE_CODE
-                                , SIZE_NAME   = @SIZE_NAME
-                                , SIZE_DESC   = @SIZE_DESC
-                                , FLAG_ROW     = @FLAG_ROW
-                                , UPDATED_BY   = @UPDATED_BY
-                                , UPDATED_DATE = @UPDATED_DATE
-                                WHERE
-                                SIZE_CODE = @SIZE_CODE;
-                                ";
-
-            var parms = new
-            {
-                SIZE_CODE = entity.SIZE_CODE,
-                SIZE_NAME = entity.SIZE_NAME,
-                SIZE_DESC = entity.SIZE_DESC,
-                FLAG_ROW = entity.FLAG_ROW,
-                UPDATED_BY = entity.UPDATED_BY,
-                UPDATED_DATE = entity.UPDATED_DATE?.ToDateTime2()
-            };
-
-            Connection.ExecuteScalar<int>(
-                sql: sqlExecute,
-                param: parms,
-                transaction: Transaction
-            );
-
+            throw new System.NotImplementedException();
         }
-
-        public void Delete(string code)
-        {
-            string sqlExecute = @"DELETE TB_M_SIZE WHERE SIZE_code = @SIZE_CODE;";
-            var parms = new { SIZE_CODE = code };
-
-            Connection.ExecuteScalar<int>(
-                sql: sqlExecute,
-                param: parms,
-                transaction: Transaction
-            );
-
-        }
-
     }
 
 }
